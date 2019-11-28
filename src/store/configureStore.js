0f9b7082269d29aca57eeabc6ed65c6e5be9fbf9
Igor Korenets <vik753@gmail.com>
2019-11-28 16:25:41 +0200

    add Chrome-Redux-Devtool to Redux configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,6 +8,7 @@ export default () => {
         combineReducers({
             expenses: expensesReducer,
             filters: filtersReducer,
-        })
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 };
