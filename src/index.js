f5bb7046a4d57fd866ee2303e1d808ac602db3a5
Igor Korenets <vik753@gmail.com>
2019-12-06 12:05:29 +0200

    removed test datas

--- src/index.js
+++ src/index.js
@@ -13,31 +13,31 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-    description: 'Water bill',
-    note: 'Water bill',
-    amount: 4500,
-    createdAt: moment('12/28/2019').format('MM/DD/YYYY').valueOf(),
-}));
-store.dispatch(addExpense({
-    description: 'Gas bill',
-    note: 'Gas bill',
-    amount: 1000,
-    createdAt: moment('12/08/2019').format('MM/DD/YYYY').valueOf(),
-}));
-store.dispatch(addExpense({
-    description: 'Rent',
-    note: 'rent',
-    amount: 109500,
-    createdAt: moment('12/27/2019').format('MM/DD/YYYY').valueOf(),
-}));
-
-store.dispatch(setTextFilter(''));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(store.getState());
-console.log(visibleExpenses);
+// store.dispatch(addExpense({
+//     description: 'Water bill',
+//     note: 'Water bill',
+//     amount: 4500,
+//     createdAt: moment('12/28/2019').format('MM/DD/YYYY').valueOf(),
+// }));
+// store.dispatch(addExpense({
+//     description: 'Gas bill',
+//     note: 'Gas bill',
+//     amount: 1000,
+//     createdAt: moment('12/08/2019').format('MM/DD/YYYY').valueOf(),
+// }));
+// store.dispatch(addExpense({
+//     description: 'Rent',
+//     note: 'rent',
+//     amount: 109500,
+//     createdAt: moment('12/27/2019').format('MM/DD/YYYY').valueOf(),
+// }));
+//
+// store.dispatch(setTextFilter(''));
+//
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(store.getState());
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
